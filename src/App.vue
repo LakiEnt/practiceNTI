21341201dfe21ede622fd8759787390155504834
LakiEnt <91592081+LakiEnt@users.noreply.github.com>
2022-11-29 11:59:54 +0500

    timer v0.2 (add style)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
   <div>
-    <div>
-      <h1>Счётчик</h1>
+    <div class="btns">
+      <h1 style="font-family: Roboto, sans-serif;">Счётчик</h1>
     </div>
-    <div>
-      <button @click="setMinute(1,0)">1 min</button>
-      <button @click="setMinute(5,0)">5 min</button>
-      <button @click="setMinute(15,0)">15 min</button>
-      <button @click="setMinute(20,0)">20 min</button>
-      <button @click="setMinute(30,0)">30 min</button>
-      <button @click="setMinute(0,1)">1 hour</button>
-      <br>
-      <br>
-      <button @click="setMinute">Your time:</button><input placeholder="Input your time">
-      <br>
-      <br>
-      <button @click="stopTimer">Stop timer</button>
-      <button @click="cleanTimer">Clean timer</button>
-      <button @click="continueTimer">Continue timer</button>
-      
+      <div>
+      <button class="btn" @click="setMinute(5, 0)">5 min</button>
+      <button class="btn" @click="setMinute(1, 0)">1 min</button>
+      <button class="btn" @click="setMinute(15, 0)">15 min</button>
+      <button class="btn" @click="setMinute(20, 0)">20 min</button>
+      <button class="btn" @click="setMinute(30, 0)">30 min</button>
+      <button class="btn" @click="setMinute(0, 1)">1 hour</button>
+      <br/>
+      <input class="input" placeholder="Input your time" />
+      <button class="btn" @click="setMinute">Your time:</button>
+      <br/>
+      <button class="btn" @click="stopTimer">Stop timer</button>
+      <button class="btn" @click="cleanTimer">Clean timer</button>
+      <button class="btn" @click="continueTimer">Continue timer</button>
     </div>
-    <h1>{{ hourTime }}:{{ minuteTime }}:{{ secondTime }}</h1>
+    <div class="time">{{ hourTime }}:{{ minuteTime }}:{{ secondTime }}</div>
   </div>
 </template>
 
@@ -29,14 +27,15 @@ export default {
   components: {},
   data() {
     return {
-      hourTime:"00",
+      hourTime: "00",
       minuteTime: "00",
       secondTime: "00",
       timer: null,
     };
   },
   methods: {
-    setMinute(minute,hour) {  //по идее наоборот
+    setMinute(minute, hour) {
+      //по идее наоборот
       //const second = 1000
       //const minute = second * 60
       //const hour = minute * 60
@@ -49,12 +48,16 @@ export default {
         this.hourTime = time.getHours();
         time.setTime(time.getTime() - 1000);
 
-        if (this.hourTime == 0 && this.minuteTime == 0 && this.secondTime == 0) { 
-          let result = (hour == 0)  ? ' минут':' час'  //синтаксические ошибки по обработке данных
-          let resultTime = (hour == 0) ? minute : hour
+        if (
+          this.hourTime == 0 &&
+          this.minuteTime == 0 &&
+          this.secondTime == 0
+        ) {
+          let result = hour == 0 ? " минут" : " час"; //синтаксические ошибки по обработке данных
+          let resultTime = hour == 0 ? minute : hour;
 
-          alert("Прошло "+ resultTime  + result);
-          this.stopTimer()
+          alert("Прошло " + resultTime + result);
+          this.stopTimer();
         }
       }, 1000);
     },
@@ -63,18 +66,45 @@ export default {
       clearInterval(this.timer);
       clearTimeout(this.timer);
     },
-    cleanTimer(){
-      this.hourTime = '00'
-      this.minuteTime ='00'
-      this.secondTime = '00'
+    cleanTimer() {
+      this.hourTime = "00";
+      this.minuteTime = "00";
+      this.secondTime = "00";
     },
-    continueTimer(){  
-      
-    },
-    
+    continueTimer() {},
   },
 };
 </script>
 
 <style>
+.btns{
+  display: flex;
+  
+}
+.btn {
+  margin: 3px;
+  font-family: Roboto, sans-serif;
+  font-weight: 0;
+  font-size: 14px;
+  color: #fff;
+  background-color: #0066cc;
+  padding: 10px 30px;
+  border: 2px solid #0066cc;
+  box-shadow: rgb(0, 0, 0) 0px 0px 0px 0px;
+  border-radius: 1px;
+  cursor: pointer;
+}
+.btn:hover{
+  background-color:#0056ac;
+}
+.input{
+  margin: 3px;
+  padding: 10px 30px;
+  border: 2px solid #0066cc;
+}
+.time{
+  font-family: Roboto, sans-serif;
+  font-size: 80px;
+  text-align: center;
+}
 </style>
